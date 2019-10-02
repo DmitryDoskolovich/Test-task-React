090e0e53558c6fad63120edd4d93b8fbd436f6c2
DmitryDoskolovich <dmitriy.doskolovich@gmail.com>
2019-10-03 00:08:25 +0400

    Fix for issue #1. Decomposition issue fix The App component renders LoginForm or UserProfile due to state isLoggedIn.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,59 @@ import React from "react";
 import Logo from "./components/Logo/Logo";
 import LoginForm from "./components/LoginForm/LoginForm";
 import "./App.css";
+import UserProfile from "./components/UserProfile/UserProfile";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      classLoginForm: "app__login-form_position app__login-form_size",
-      classLogo: "app__logo_position"
+      isLoggedIn: false,
+      avatarUrl: "",
+      userName: ""
     };
+    this.loginSuccessful = this.loginSuccessful.bind(this);
+    this.handleOnClickLogout = this.handleOnClickLogout.bind(this);
   }
+
+  handleOnClickLogout = e =>
+    this.setState({
+      isLoggedIn: false,
+      avatarUrl: "",
+      userName: ""
+    });
+
+  loginSuccessful = responseObj => {
+    this.setState({ isLoggedIn: true });
+    this.setState({ avatarUrl: responseObj["photoUrl"] });
+    this.setState({ userName: responseObj["name"] });
+  };
+
   render() {
     return (
-      <div className="authorization-page authorization-page_default page__authorization-page_position page__authorization-page_size ">
-        <Logo className={this.state.classLogo} />
-        <LoginForm className={this.state.classLoginForm} />
+      <div className="authorization-page">
+        <Logo className={this.props.classLogo} />
+        {this.state.isLoggedIn ? (
+          <UserProfile
+            className={this.props.classUserProfile}
+            avatarUrl={this.state.avatarUrl}
+            userName={this.state.userName}
+            onClick={this.handleOnClickLogout}
+          />
+        ) : (
+          <LoginForm
+            className={this.props.classLoginForm}
+            loginSuccessful={this.loginSuccessful}
+          />
+        )}
       </div>
     );
   }
 }
+
+App.defaultProps = {
+  classLoginForm: "app__login-form_position app__login-form_size",
+  classLogo: "app__logo_position",
+  classUserProfile: "app__user-profile_size app__user-profile_position "
+};
 
 export default App;
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,36 +1,21 @@
 import React from "react";
 import "./LoginForm.css";
-import UserProfile from "../UserProfile/UserProfile";
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: false,
       isAuthError: false,
       email: "",
-      password: "",
-      avatarUrl: "",
-      userName: "",
-      classUserProfile:
-        "login-form__user-profile_size login-form__user-profile_position"
+      password: ""
     };
     this.handleChangeValueEmail = this.handleChangeValueEmail.bind(this);
     this.handleChangeValuePassword = this.handleChangeValuePassword.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleOnClickLogout = this.handleOnClickLogout.bind(this);
   }
 
   handleChangeValueEmail = e => this.setState({ email: e.target.value });
   handleChangeValuePassword = e => this.setState({ password: e.target.value });
-
-  handleOnClickLogout = e =>
-    this.setState({
-      isLoggedIn: false,
-      isAuthError: false,
-      email: "",
-      password: ""
-    });
 
   handleSubmit(event) {
     event.preventDefault();
@@ -49,9 +34,7 @@ class LoginForm extends React.Component {
         } else {
           res.json().then(response => {
             let responseObj = response;
-            this.setState({ avatarUrl: responseObj["photoUrl"] });
-            this.setState({ userName: responseObj["name"] });
-            this.setState({ isLoggedIn: true });
+            this.props.loginSuccessful(responseObj);
           });
         }
       })
@@ -59,59 +42,47 @@ class LoginForm extends React.Component {
   }
 
   render() {
-    const isLoggedIn = this.state.isLoggedIn;
     const isAuthError = this.state.isAuthError;
     return (
-      <div>
-        {isLoggedIn ? (
-          <UserProfile
-            className={this.state.classUserProfile}
-            avatarUrl={this.state.avatarUrl}
-            userName={this.state.userName}
-            onClick={this.handleOnClickLogout}
+      <form
+        className={`login-form ${this.props.className}`}
+        onSubmit={this.handleSubmit}
+      >
+        <div className="login-form__content">
+          <h3 className="login-form__header">Log In</h3>
+          <input
+            className={
+              isAuthError
+                ? "login-form__input login-form__input_error"
+                : "login-form__input"
+            }
+            name="email"
+            type="email"
+            placeholder="E-Mail"
+            value={this.state.email}
+            onChange={this.handleChangeValueEmail}
           />
-        ) : (
-          <form
-            className={`login-form ${this.props.className}`}
-            onSubmit={this.handleSubmit}
-          >
-            <div className="login-form__content">
-              <h3 className="login-form__header">Log In</h3>
-              <input
-                className={
-                  isAuthError
-                    ? "login-form__input login-form__input_error"
-                    : "login-form__input"
-                }
-                name="email"
-                type="email"
-                placeholder="E-Mail"
-                value={this.state.email}
-                onChange={this.handleChangeValueEmail}
-              />
-              <input
-                className="login-form__input"
-                name="password"
-                type="password"
-                placeholder="Password"
-                value={this.state.password}
-                onChange={this.handleChangeValuePassword}
-              />
-              {isAuthError ? (
-                <p className="login-form__error-message">
-                  E-Mail or password is incorrect
-                </p>
-              ) : null}
-              <input
-                className="login-form__button"
-                type="submit"
-                name="submit"
-                value="Login"
-              />
-            </div>
-          </form>
-        )}
-      </div>
+          <input
+            className="login-form__input"
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChangeValuePassword}
+          />
+          {isAuthError ? (
+            <p className="login-form__error-message">
+              E-Mail or password is incorrect
+            </p>
+          ) : null}
+          <input
+            className="login-form__button"
+            type="submit"
+            name="submit"
+            value="Login"
+          />
+        </div>
+      </form>
     );
   }
 }
