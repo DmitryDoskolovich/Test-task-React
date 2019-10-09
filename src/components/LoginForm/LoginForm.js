db5e29b7b5c12e879d4712c6ea04c3de9bca79b7
DmitryDoskolovich <54312942+DmitryDoskolovich@users.noreply.github.com>
2019-10-09 19:13:26 +0400

    Revert "Fix 1 issue"

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,21 +1,35 @@
 import React from "react";
 import "./LoginForm.css";
-import classNames from "classnames";
 
-class LoginForm extends React.Component {
+class LoginForm2 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoggedIn: false,
       isAuthError: false,
       email: "",
-      password: ""
+      password: "",
+      avatarUrl: "",
+      avatarName: ""
     };
+    this.handleChangeValueEmail = this.handleChangeValueEmail.bind(this);
+    this.handleChangeValuePassword = this.handleChangeValuePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOnClickLogout = this.handleOnClickLogout.bind(this);
   }
 
   handleChangeValueEmail = e => this.setState({ email: e.target.value });
   handleChangeValuePassword = e => this.setState({ password: e.target.value });
 
-  handleSubmit = event => {
+  handleOnClickLogout = e =>
+    this.setState({
+      isLoggedIn: false,
+      isAuthError: false,
+      email: "",
+      password: ""
+    });
+
+  handleSubmit(event) {
     event.preventDefault();
     let credentials = `{"email":"${this.state.email}","password":"${this.state.password}"}`;
     const url = "https://us-central1-mercdev-academy.cloudfunctions.net/login";
@@ -32,57 +46,77 @@ class LoginForm extends React.Component {
         } else {
           res.json().then(response => {
             let responseObj = response;
-            this.props.loginSuccessful(responseObj);
+            this.setState({ avatarUrl: responseObj["photoUrl"] });
+            this.setState({ avatarName: responseObj["name"] });
+            this.setState({ isLoggedIn: true });
           });
         }
       })
       .catch(error => console.error("Fetch error", error));
-  };
+  }
 
   render() {
+    const isLoggedIn = this.state.isLoggedIn;
     const isAuthError = this.state.isAuthError;
     return (
       <form
-        className={classNames("login-form", this.props.className)}
+        className="login-form login-form_default app__login-form_position app__login-form_size"
         onSubmit={this.handleSubmit}
       >
-        <div className="login-form__content">
-          <h3 className="login-form__header">Log In</h3>
-          <input
-            className={
-              isAuthError
-                ? "login-form__input login-form__input_error"
-                : "login-form__input"
-            }
-            name="email"
-            type="email"
-            placeholder="E-Mail"
-            value={this.state.email}
-            onChange={this.handleChangeValueEmail}
-          />
-          <input
-            className="login-form__input"
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleChangeValuePassword}
-          />
-          {isAuthError ? (
-            <p className="login-form__error-message">
-              E-Mail or password is incorrect
-            </p>
-          ) : null}
-          <input
-            className="login-form__button"
-            type="submit"
-            name="submit"
-            value="Login"
-          />
-        </div>
+        {isLoggedIn ? (
+          <div className="login-form__content login-form__content_logged-in">
+            <img
+              className="login-form__avatar"
+              src={this.state.avatarUrl}
+              alt="avatar"
+            />
+            <p className="login-form__avatar-name">{this.state.avatarName}</p>
+            <input
+              className="login-form__button login-form__button_logoutbtn"
+              type="submit"
+              value="Logout"
+              onClick={this.handleOnClickLogout}
+            ></input>
+          </div>
+        ) : (
+          <div className="login-form__content login-form__content_not-logged-in">
+            <h3 className="login-form__header">Log In</h3>
+            <input
+              className={
+                isAuthError
+                  ? "login-form__input login-form__input_email login-form__email_error"
+                  : "login-form__input login-form__input_email"
+              }
+              name="email"
+              type="email"
+              placeholder="E-Mail"
+              value={this.state.email}
+              onChange={this.handleChangeValueEmail}
+            />
+            <input
+              className="login-form__input login-form__input_password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChangeValuePassword}
+            />
+            {isAuthError ? (
+              <p className="login-form__error-message">
+                E-Mail or password is incorrect
+              </p>
+            ) : null}
+            <input
+              className="login-form__button login-form__button_loginbtn"
+              type="submit"
+              name="submit"
+              value="Login"
+            />
+          </div>
+        )}
       </form>
     );
   }
 }
 
-export default LoginForm;
+export default LoginForm2;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./Logo.css";
-import classNames from "classnames";
 
 class Logo extends React.Component {
   render() {
     return (
-      <div className={classNames("logo", this.props.className)}>
+      <div className="logo app__logo_position">
         <img className="logo__img" src={logo} alt="logo" />
       </div>
     );
